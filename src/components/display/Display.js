a965543b7a989600c0a4c71b086ad360bed4812d
Liviu Ioan Iancu <liviu.iancu@tim-mb-150.3pillar.corp>
2018-03-30 11:21:41 +0300

    Scale text to fit display width

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Display.css';
+import AutoScalingText from './auto-scaling-text/AutoScalingText'
 
 export default class Display extends Component {
   render(){
     return(
       <div className="input-container">
-        <div>{this.props.value}</div>
+          <AutoScalingText>{this.props.value}</AutoScalingText>
       </div>
     )
   }
