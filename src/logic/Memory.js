2ef395de592f832a310254104e7412c55433a705
Liviu Ioan Iancu <liviu.iancu@tim-mb-150.3pillar.corp>
2018-04-04 11:57:59 +0300

    Subtract each new memory number from previous one

--- src/logic/Memory.js
+++ src/logic/Memory.js
@@ -5,13 +5,19 @@ export function memoryAdd(numberArray, memory) {
   const currentNumber = +numberArray[numberArray.length - 1];
   let updateMemory = memory + currentNumber;
   if(currentNumber === '0') updateMemory = memory;
-  
+
   return {
     memory: updateMemory
   }
 }
-export function memorySubtract() {
-  console.log('subtract')
+export function memorySubtract(numberArray, memory) {
+  const currentNumber = +numberArray[numberArray.length - 1];
+  let updateMemory = memory - currentNumber;
+  if(currentNumber === '0') updateMemory = memory;
+  
+  return {
+    memory: updateMemory
+  }
 }
 export function memoryRecall() {
   console.log('recall')
